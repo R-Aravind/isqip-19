ba70ed3e30997ec43293180dfada95570438859d
Allen Francis <lallenfrancisl@gmail.com>
2019-05-25 16:46:01 +0530

    add text reveal animation to contacts page     * add screen reader only utility class to main.css     * add dependency animejs

--- ISQIP-19/resources/js/main.js
+++ ISQIP-19/resources/js/main.js
@@ -1,26 +1,19 @@
-
 // Router
-window.onload = ()=> Route({
-    '': 'banner.html',
-    '/contact': 'contact.html',
-    '/faq': 'faq.html',
-    '/isqip': 'isqip.html',
-    '/ticket': 'ticket.html',
-});
-/////
+window.onload = () => Route({'': 'banner.html', '/contact': 'contact.html', '/faq': 'faq.html', '/isqip': 'isqip.html', '/ticket': 'ticket.html'});
+// ///
 
 
 // SIDE BAR MOBILE
-$(document).ready(()=> {
-    $('#sidebar-btn').click(()=>{
+$(document).ready(() => {
+    $('#sidebar-btn').click(() => {
         $(".sidebar").fadeIn(300);
-        $(".mob-topbar, .content-side").css({opacity:0.5});
+        $(".mob-topbar, .content-side").css({opacity: 0.5});
     });
-    $(window).on('resize', function(){
+    $(window).on('resize', function () {
         var win = $(window);
         if (win.width() >= 575.98) {
             $(".sidebar").fadeIn(300);
-            $(".mob-topbar, .content-side").css({opacity:1});
+            $(".mob-topbar, .content-side").css({opacity: 1});
         }
         if (win.width() < 575.98) {
             $(".sidebar").fadeOut(300);
@@ -28,39 +21,77 @@ $(document).ready(()=> {
     });
 
     // side bar active change
-    $(window).on('hashchange load', ()=>{
+    $(window).on('hashchange load', () => {
         let currentPage = window.location.hash.substring(1).slice(1);
-        if(currentPage == ''){
+        if (currentPage == '') {
             $('.menu li').removeClass('active');
             $('.menu li div.selected').remove();
             $('.home-btn').append('<div class="selected"></div>');
-        }else{
+        } else {
             $('.menu li').removeClass('active');
             $('.menu li div.selected').remove();
-            currentPage ='.' + currentPage + '-btn';
+            currentPage = '.' + currentPage + '-btn';
             $(currentPage).append('<div class="selected"></div>');
         }
     });
 
 });
 $(document).mouseup(function (e) {
-        if($(window).width() < 575.98){
-            if (!$('.sidebar').is(e.target) && $('.sidebar').has(e.target).length === 0){
-                $('.sidebar').fadeOut();
-                $(".mob-topbar, .content-side").css({opacity:1});
+    if ($(window).width() < 575.98) {
+        if (!$('.sidebar').is(e.target) && $('.sidebar').has(e.target).length === 0) {
+            $('.sidebar').fadeOut();
+            $(".mob-topbar, .content-side").css({opacity: 1});
         }
     }
 });
-/////////////
+// ///////////
 
+/**
+ * contact page global variables
+ */
+var textCurtains = null;
+var textToReveal = null;
+var textCurtainEffect = null;
+// Page Scripts //
+let pageScript = () => { // FAQ PAGE
+    $('.faq-question').click(function () {
 
-//Page Scripts //
-let pageScript = () => {
+        $(this).next('.faq-answer').fadeToggle();
+    });
 
-    // FAQ PAGE
-    $('.faq-question').click(function (){
+    /**
+     * Contact page scripts
+     */
+    // var contactBtn = $('.contact-btn');
 
-        $(this).next('.faq-answer').fadeToggle(); 
+    $('.contact-btn').click(function () {
+        // text curtain reveal animation
+        textCurtains = document.getElementsByClassName('text-reveal-curtain');
+        textToReveal = document.getElementsByClassName('text-to-reveal');
+        textCurtainEffect = anime.timeline({
+            easing: 'easeInOutSine',
+            duration: 100,
+            delay: anime.stagger(50),
+            endDelay: 50,
+            autoplay: false
+        });
+
+        textCurtainEffect
+        .add({
+            targets: textCurtains,
+            scaleX: [0, 1],
+            direction: 'alternate',
+            complete: function () {
+                for (var i = 0; i < textToReveal.length; ++ i) {
+                    textToReveal[i].classList.toggle('make-invisible');
+                    textCurtains[i].style.transformOrigin = 'right';
+                }
+            }
+        })
+        .add({
+            targets: textCurtains,
+            scaleX: [1, 0]
+        });
     });
 };
-////////////////
+// //////////////
