4fe757356a32a577ffa298f9115d8dc7e719f3ec
R Aravind <aravind.ar786@gmail.com>
2019-05-25 21:26:35 +0530

    Revert "plan B"
    This reverts commit b6638f006bb858da24efa2c1a0bfc77258c70a29.

---
+++ BRANCHES/CS-ISQIP/resources/js/main.js
@@ -0,0 +1,66 @@
+
+// Router
+window.onload = ()=> Route({
+    '': 'banner.html',
+    '/workshops':'workshops.html',
+    '/schedule':'schedule.html',
+    '/speakers':'speakers.html',
+    '/ticket': 'ticket.html',
+});
+/////
+
+
+// SIDE BAR MOBILE
+$(document).ready(()=> {
+    $('#sidebar-btn').click(()=>{
+        $(".sidebar").fadeIn(300);
+        $(".mob-topbar, .content-side").css({opacity:0.5});
+    });
+    $(window).on('resize', function(){
+        var win = $(window);
+        if (win.width() >= 575.98) {
+            $(".sidebar").fadeIn(300);
+            $(".mob-topbar, .content-side").css({opacity:1});
+        }
+        if (win.width() < 575.98) {
+            $(".sidebar").fadeOut(300);
+        }
+    });
+
+    // side bar active change
+    $(window).on('hashchange load', ()=>{
+        let currentPage = window.location.hash.substring(1).slice(1);
+        if(currentPage == ''){
+            $('.menu li').removeClass('active');
+            $('.menu li div.selected').remove();
+            $('.home-btn').append('<div class="selected"></div>');
+        }else{
+            $('.menu li').removeClass('active');
+            $('.menu li div.selected').remove();
+            currentPage ='.' + currentPage + '-btn';
+            $(currentPage).append('<div class="selected"></div>');
+        }
+    });
+
+});
+$(document).mouseup(function (e) {
+        if($(window).width() < 575.98){
+            if (!$('.sidebar').is(e.target) && $('.sidebar').has(e.target).length === 0){
+                $('.sidebar').fadeOut();
+                $(".mob-topbar, .content-side").css({opacity:1});
+        }
+    }
+});
+/////////////
+
+
+//Page Scripts //
+let pageScript = () => {
+
+    // FAQ PAGE
+    $('.faq-question').click(function (){
+
+        $(this).next('.faq-answer').fadeToggle(); 
+    });
+};
+////////////////
